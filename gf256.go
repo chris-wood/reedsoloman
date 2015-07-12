05c0562d9f0e7059520a223e4e553e7c4fb84e53
Christopher Wood <christopherwood07@gmail.com>
2015-07-11 19:59:08 -0700

    rawr

--- gf256.go
+++ gf256.go
@@ -6,14 +6,17 @@ func (f *Field) Add(x byte, y byte) byte {
 	return x ^ y
 }
 
-func (f *Field) Mul(x byte, y byte) byte {
+func (f *Field) Mul(x int, y int, poly int) int {
     z := 0
     for x > 0 {
         if (x & 1) != 0 {
-            z += y
+            z ^= y
         }
         x >>= 1
         y <<= 1
+        if (y & 0x100) != 0 {
+            y ^= poly
+        }
     }
     return z
 }
