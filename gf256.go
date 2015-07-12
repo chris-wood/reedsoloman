2dd92ae3d37deb1417c6662bc0d1e8466ee405de
Christopher Wood <christopherwood07@gmail.com>
2015-07-11 18:41:55 -0700

    mul

--- gf256.go
+++ gf256.go
@@ -6,4 +6,16 @@ func (f *Field) Add(x byte, y byte) byte {
 	return x ^ y
 }
 
+func (f *Field) Mul(x byte, y byte) byte {
+    z := 0
+    for x > 0 {
+        if (x & 1) != 0 {
+            z += y
+        }
+        x >>= 1
+        y <<= 1
+    }
+    return z
+}
+
 
