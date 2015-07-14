3c42899afcf4461e4d9d7f8d16ef25f9cf6fa4a0
Christopher Wood <christopherwood07@gmail.com>
2015-07-14 16:27:36 -0700

    Added encoder and decoder

---
+++ src/ecc/encoder.go
@@ -0,0 +1,22 @@
+package main
+
+import "./field"
+import "os"
+import "fmt"
+import "io"
+import "io/ioutil"
+import "log"
+
+func main() {
+	fmt.Println("Encoder...")
+	args := os.Args[1:]
+
+	fname = args[0]
+	data, err := ioutil.ReadFile(fname)
+	if err != nil {
+		// TODO: give data to the encoder
+		fmt.Print(data)
+	} else {
+		log.Fatal("Error reading the file %s", string(err))
+	}
+}
